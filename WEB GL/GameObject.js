d30d9353c2dc54d3e3830ec333c380acc7938da3
emanuele_rispoli_2712 <emanuelerispoli2712@gmail.com>
2025-08-22 12:39:04 +0200

    Organizzata classe Transform e Gameobject

--- WEB GL/GameObject.js
+++ WEB GL/GameObject.js
@@ -2,36 +2,19 @@ class GameObject {
     constructor(mesh) {
         this.mesh = mesh;
         this.Color = [1.0, 1.0, 1.0, 1.0];
-        this.modelMatrix = mat4_.create();  // identità
+        this.transform = new Transform();
         console.log("GameObject Class", this);
-    }
-
-    setPosition(x, y, z) {
-        mat4_.fromTranslation(this.modelMatrix, [x, y, z]);
-    }
-
-    setRotation(angleRad, axis) {
-        mat4_.fromRotation(this.modelMatrix, angleRad, axis);
-    }
-
-    setScale(sx, sy, sz) {
-        mat4_.fromScaling(this.modelMatrix, [sx, sy, sz]);
     }
 
     Draw(attribs, uniforms) {
         const gl = this.mesh.gl;
-        if(!gl) return;
-        // bind buffers della mesh
+        if (!gl) return;
+
         this.mesh.BindVertexBuffer(attribs.aPosition);
-        // bind colori
         this.mesh.BindColorBuffer(attribs.aColor);
-        // bind indices
         this.mesh.BindIndicesBuffer();
 
-        // imposta uniform
-        // gl.uniform4fv(uniforms.uGlobalColor, this.Color);
-        gl.uniformMatrix4fv(uniforms.uModelMatrix, false, this.modelMatrix);
-        // draw
+        gl.uniformMatrix4fv(uniforms.uModelMatrix, false, this.transform.modelMatrix);
         gl.drawElements(gl.TRIANGLES, this.mesh.indecesData.length, gl.UNSIGNED_SHORT, 0);
     }
 }
--- WEB GL/Scene.js
+++ WEB GL/Scene.js
@@ -10,15 +10,16 @@ class Scene {
         this.renderEntity.Render();
     }
     setupGameObjects() {
+        this.gameObjects = [];
         const gl = this.renderEntity.gl;
         const firstSquare = new GameObject(new SquareMesh3D(gl));
-        firstSquare.setPosition(-2, 0, 0);
+        firstSquare.transform.setPosition(-2, 0, 0);
         
         const firstTriangle = new GameObject(new PyramidMesh3D(gl));
-        firstTriangle.setPosition(0, 0, 0);
+        firstTriangle.transform.setPosition(0, 0, 0);
 
         const secondSquare = new GameObject(new SquareMesh3D(gl));
-        secondSquare.setPosition(2, 0, 0);
+        secondSquare.transform.setPosition(2, 0, 0);
         this.gameObjects = [
             firstSquare,
             firstTriangle,
